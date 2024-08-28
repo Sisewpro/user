2e3514795ff5f06f4c8e1cb41ff3ccba5c8a9d6b
Az Zikra Ismunandar <azzikra.ismunandar08@gmail.com>
2024-08-28 13:55:18 +0700

    wu planachii

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -49,8 +49,10 @@
                 </div>
             </div>
             <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                <button type="submit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-600 sm:ml-3 sm:w-auto sm:text-sm">Save</button>
-                <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 sm:mt-0 sm:w-auto sm:text-sm" onclick="toggleModal()">Cancel</button>
+                <x-primary-button class="ms-3">Save</x-primary-button>
+                <x-secondary-button x-on:click="$dispatch('close')" >
+                    {{ __('Cancel') }}
+                </x-secondary-button>
             </div>
         </form>
     </x-modal>
