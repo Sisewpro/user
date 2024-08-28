50aab3a472422a10607270e9cf174a7371f715fd
Az Zikra Ismunandar <azzikra.ismunandar08@gmail.com>
2024-08-28 13:25:46 +0700

    New Paracetamol
    Comunicados de prensa

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,13 +13,13 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('LOCKETS') }}
+                        {{ __('Lockets') }}
                     </x-nav-link>
                 </div>
 
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('counttime')" :active="request()->routeIs('counttime')">
-                        {{ __('DATA MASTER') }}
+                        {{ __('Master Data') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -30,7 +30,7 @@
                     <x-slot name="trigger">
                         <button
                             class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div class="uppercase">{{ Auth::user()->name }}</div>
+                            <div>{{ Auth::user()->name }}</div>
 
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
